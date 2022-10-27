763d6d22eff1dc71a173f4888a52da4a30e94d96
mohammed-webdev <mas.webdev24@gmail.com>
2022-10-27 19:28:45 +0200

    change source imges

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,13 +1,19 @@
 import React, { Fragment } from 'react'
 import Cart from '../components/Cart'
+import imgOne from "../../public/images/1.png"
+import imgTwo from "../../public/images/2.png"
+import imgThree from "../../public/images/3.png"
+import imgFour from "../../public/images/4.png"
+import imgFive from "../../public/images/5.png"
+import imgSix from "../../public/images/6.png"
 
 const persons = [
-    {id:1,work:"Director",name:"Ali",img:"public/images/1.png",about:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aut provident sapiente reprehenderit eaque voluptatem dicta deserunt distinctio sequi cumque."},
-    {id:2,work:"Co-Director",name:"Marwa",img:"public/images/2.png",about:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aut provident sapiente reprehenderit eaque voluptatem dicta deserunt distinctio sequi cumque."},
-    {id:3,work:"Promotional Matters",name:"Haneen",img:"public/images/3.png",about:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aut provident sapiente reprehenderit eaque voluptatem dicta deserunt distinctio sequi cumque."},
-    {id:4,work:"Marketing Director",name:"Hussin",img:"public/images/4.png",about:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aut provident sapiente reprehenderit eaque voluptatem dicta deserunt distinctio sequi cumque."},
-    {id:5,work:"Coordinator",name:"Adnan",img:"public/images/5.png",about:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aut provident sapiente reprehenderit eaque voluptatem dicta deserunt distinctio sequi cumque."},
-    {id:6,work:"Director Of Photography",name:"Abdallah",img:"public/images/6.png",about:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aut provident sapiente reprehenderit eaque voluptatem dicta deserunt distinctio sequi cumque."}
+    {id:1,work:"Director",name:"Ali",img:imgOne,about:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aut provident sapiente reprehenderit eaque voluptatem dicta deserunt distinctio sequi cumque."},
+    {id:2,work:"Co-Director",name:"Marwa",img:imgTwo,about:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aut provident sapiente reprehenderit eaque voluptatem dicta deserunt distinctio sequi cumque."},
+    {id:3,work:"Promotional Matters",name:"Haneen",img:imgThree,about:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aut provident sapiente reprehenderit eaque voluptatem dicta deserunt distinctio sequi cumque."},
+    {id:4,work:"Marketing Director",name:"Hussin",img:imgFour,about:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aut provident sapiente reprehenderit eaque voluptatem dicta deserunt distinctio sequi cumque."},
+    {id:5,work:"Coordinator",name:"Adnan",img:imgFive,about:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aut provident sapiente reprehenderit eaque voluptatem dicta deserunt distinctio sequi cumque."},
+    {id:6,work:"Director Of Photography",name:"Abdallah",img:imgSix,about:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aut provident sapiente reprehenderit eaque voluptatem dicta deserunt distinctio sequi cumque."}
 ]
 export default function Team() {
   return (
